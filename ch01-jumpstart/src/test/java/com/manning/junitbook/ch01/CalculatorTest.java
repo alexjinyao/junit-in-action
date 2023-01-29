d783709f59411765cab61a7b61cd283b09b1e318
jiny75 <jiny75@chinaunicom.cn>
2023-01-29 09:36:40 +0800

    finish chapter1

---
+++ ch01-jumpstart/src/test/java/com/manning/junitbook/ch01/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.manning.junitbook.ch01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        double result = calculator.add(10, 50);
+        assertEquals(60, result, 0);
+    }
+}
