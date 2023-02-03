7e43b4dc02a3f27e07a13fb37db440d25caec731
jiny75 <jiny75@chinaunicom.cn>
2023-02-03 09:54:40 +0800

    working on chapter 8 mock, finish demo of EasyMock

--- ch08-mocks/src/test/java/com/manning/junitbook/ch08/web/TestWebClientEasyMock.java
+++ ch08-mocks/src/test/java/com/manning/junitbook/ch08/web/TestWebClientEasyMock.java
@@ -1,4 +1,90 @@
 package com.manning.junitbook.ch08.web;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.easymock.EasyMock.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+/**
+ * Test the WebClient
+ */
 public class TestWebClientEasyMock {
+    private ConnectionFactory factory;
+
+    private InputStream stream;
+
+    @BeforeEach
+    void setUp(){
+        factory = createMock("factory", ConnectionFactory.class);
+        stream = createMock("stream", InputStream.class);
+    }
+
+    @Test
+    void testGetContentOk() throws Exception {
+        expect(factory.getData()).andReturn(stream);
+        expect(stream.read()).andReturn(Integer.valueOf((byte)'W'));
+        expect(stream.read()).andReturn(Integer.valueOf((byte) 'o'));
+        expect(stream.read()).andReturn(Integer.valueOf((byte) 'r'));
+        expect(stream.read()).andReturn(Integer.valueOf((byte) 'k'));
+        expect(stream.read()).andReturn(Integer.valueOf((byte) 's'));
+        expect(stream.read()).andReturn(Integer.valueOf((byte) '!'));
+
+        expect(stream.read()).andReturn(-1);
+        stream.close();
+
+        replay(factory);
+        replay(stream);
+
+        WebClient2 client = new WebClient2();
+
+        String workingContent = client.getContent(factory);
+
+        assertEquals("Works!", workingContent);
+    }
+
+    @Test
+    void testGetContentInputStreamNull() throws Exception {
+        expect(factory.getData()).andReturn(null);
+
+        replay(factory);
+        replay(stream);
+
+        WebClient2 client = new WebClient2();
+
+        String workingContent = client.getContent(factory);
+
+        assertNull(workingContent);
+    }
+
+    @Test
+    void testGetContentCannotCloseInputStream() throws Exception {
+        expect(factory.getData()).andReturn(stream);
+        expect(stream.read()).andReturn(-1);
+
+        // We add another test to simulate a condition when we cannot close the InputStream.
+        // We define an expectation in which we expect the close method of the stream to be invoked.
+        stream.close();
+        // We declare that an IOExpectation should be raised
+        expectLastCall().andThrow(new IOException("cannot close"));
+
+        replay(factory);
+        replay(stream);
+
+        WebClient2 client = new WebClient2();
+        String workingContent = client.getContent(factory);
+
+        assertNull(workingContent);
+    }
+
+    @AfterEach
+    void tearDown(){
+        verify(factory);
+        verify(stream);
+    }
 }
