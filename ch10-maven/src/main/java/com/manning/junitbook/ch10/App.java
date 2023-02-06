d2cbbab372879d715723d84c03b70d3753405228
jiny75 <jiny75@chinaunicom.cn>
2023-02-06 10:45:39 +0800

    finish chapter 10 maven

---
+++ ch10-maven/src/main/java/com/manning/junitbook/ch10/App.java
@@ -0,0 +1,7 @@
+package com.manning.junitbook.ch10;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ ch10-maven/src/test/java/com/manning/junitbook/ch10/AppTest.java
@@ -0,0 +1,16 @@
+package com.manning.junitbook.ch10;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
