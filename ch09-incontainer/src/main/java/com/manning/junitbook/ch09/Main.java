d307705138217dc9d1e71a274f3fcdfabeabd1ba
jiny75 <jiny75@chinaunicom.cn>
2023-02-06 10:10:55 +0800

    finish chapter 9

--- ch09-incontainer/src/main/java/com/manning/junitbook/ch09/Main.java
+++
@@ -1,7 +0,0 @@
-package com.manning.junitbook.ch09;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- ch09-incontainer/src/main/java/com/manning/junitbook/ch09/airport/Flight.java
+++ ch09-incontainer/src/main/java/com/manning/junitbook/ch09/airport/Flight.java
@@ -33,5 +33,19 @@ public class Flight {
         return passengers.size();
     }
 
+    public boolean addPassenger(Passenger passenger) {
+        if (passengers.size() >= seats) {
+            throw new RuntimeException("Cannot add more passengers than the capacity of the flight!");
+        }
+        return passengers.add(passenger);
+    }
 
+    public boolean removePassenger(Passenger passenger) {
+        return passengers.remove(passenger);
+    }
+
+    @Override
+    public String toString() {
+        return "Flight " + getFlightNumber();
+    }
 }
