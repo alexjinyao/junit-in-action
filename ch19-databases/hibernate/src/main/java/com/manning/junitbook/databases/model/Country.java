8f631c41bf697dc2202437af13a77546cb66025f
jiny75 <jiny75@chinaunicom.cn>
2023-02-10 11:41:29 +0800

    Finish chapter 19, hibernate test

---
+++ ch19-databases/hibernate/src/main/java/com/manning/junitbook/databases/model/Country.java
@@ -0,0 +1,67 @@
+package com.manning.junitbook.databases.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+/**
+ * @Entity represent objects in a database.
+ * The corresponding table in the database is provided by the @Table annotation and is named COUNTRY.
+ */
+@Entity
+@Table(name = "COUNTRY")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private int id;
+
+    private String name;
+
+    private String codeName;
+
+    public Country() {};
+
+    public Country(String name, String codeName) {
+        this.name = name;
+        this.codeName = codeName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCodeName() {
+        return codeName;
+    }
+
+    public void setCodeName(String codeName) {
+        this.codeName = codeName;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+                "name='" + name + '\'' +
+                ", codeName='" + codeName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Country country = (Country) o;
+        return Objects.equals(name, country.name) &&
+                Objects.equals(codeName, country.codeName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, codeName);
+    }
+}
