c46272ec9658ee538554557b01dc41645d3552de
jiny75 <jiny75@chinaunicom.cn>
2023-02-15 10:05:43 +0800

    Working on Chapter 20, phase 1 finish

---
+++ ch20-tdd/Before/src/main/java/com/manning/junitbook/airport/Flight.java
@@ -0,0 +1,52 @@
+package com.manning.junitbook.airport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Flight {
+
+    private String id;
+    private List<Passenger> passengers = new ArrayList<>();
+    private String flightType;
+
+    public Flight(String id, String flightType) {
+        this.id = id;
+        this.flightType = flightType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public List<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public boolean addPassenger(Passenger passenger) {
+        switch (flightType) {
+            case "Economy":
+                return passengers.add(passenger);
+            case "Business":
+                if (passenger.isVip()) {
+                    return passengers.add(passenger);
+                }
+                return false;
+            default:
+                throw new RuntimeException("Unknown type: " + flightType);
+        }
+    }
+
+    public boolean removePassenger(Passenger passenger) {
+        switch (flightType) {
+            case "Economy":
+                if (!passenger.isVip()) {
+                    return passengers.remove(passenger);
+                }
+                return false;
+            case "Business":
+                return false;
+            default:
+                throw new RuntimeException("Unknown type: " + flightType);
+        }
+    }
+}
